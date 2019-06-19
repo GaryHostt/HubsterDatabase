bf628115e98cddfe4286231942fa44318bdea498
Work_laptop_Alex <alexmacdon94@gmail.com>
2019-06-19 16:38:52 -0700

    added spanish/testing branches

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ app = Flask(__name__)
 
 print("Welcome to the Hubster Database API, Bienvenue a la database des hubsters")
 print("This API is now connected with a Jenkins CI/CD Pipeline")
+print("Bienvenidos a la API")
 
 # update below with your db credentials
 # put your wallet files in a /wallet/network/admin in the project directory
